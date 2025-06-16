b23576a6eba82add0a7ac426cc9098f7b0fd6f23
Himanshusingh9647 <himanshusingh1088@gmail.com>
2025-06-16 11:01:37 +0530

    trying myself

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // For Vercel deployment
-  css: {
-    postcss: './postcss.config.js',
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        assetFileNames: 'assets/[name]-[hash][extname]'
-      }
-    },
-    cssCodeSplit: false
-  }
-})
+  base: '/Porfolio', // For Vercel deployment
+
+});
